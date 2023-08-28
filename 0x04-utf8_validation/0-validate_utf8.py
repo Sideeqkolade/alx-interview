9c76250e068aa08138053242d14b1822031a052c
Sideeqkolade <sideeqkolade@gmail.com>
2023-08-28 01:49:32 +0100

    task 0 completed

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,23 +6,19 @@ def validUTF8(data) -> bool:
     """ method that determines if a given data set represents a
         valid UTF-8 encoding.
     """
-    bytes_left = 0
-
-    for num in data:
-        if bytes_left == 0:
-            if num >> 7 == 0b0:  # 1-byte character
-                bytes_left = 0
-            elif num >> 5 == 0b110:  # 2-byte character
-                bytes_left = 1
-            elif num >> 4 == 0b1110:  # 3-byte character
-                bytes_left = 2
-            elif num >> 3 == 0b11110:  # 4-byte character
-                bytes_left = 3
-            else:
+    num_bytes = 0
+    for byte in data:
+        mask = 1 << 7
+        if not num_bytes:
+            while byte & mask:
+                num_bytes += 1
+                mask >>= 1
+            if not num_bytes:
+                continue
+            if num_bytes == 1 or num_bytes > 4:
                 return False
         else:
-            if num >> 6 != 0b10:  # Continuation byte
+            if byte >> 6 != 0b10:
                 return False
-        bytes_left -= 1
-
-    return bytes_left == 0
+        num_bytes -= 1
+    return num_bytes == 0
